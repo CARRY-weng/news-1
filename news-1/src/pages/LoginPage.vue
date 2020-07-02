4187e5ad172895b6137fdf3e66b07a44feb44c45
CARRY-weng <53970982+CARRY-weng@users.noreply.github.com>
2020-07-02 18:14:55 +0800

    文件夹整理

---
+++ news-1/src/pages/LoginPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    <div class="closeBtn">
+      <span class="iconfont iconicon-test"></span>
+    </div>
+    <div class="logo"></div>
+    <div class="inputName"></div>
+    <div class="inputPwd"></div>
+    <div class="btn"></div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="less" scoped>
+.closeBtn{
+  .iconfont{
+    font-size: 7.5vw;
+  }
+}
+</style>
