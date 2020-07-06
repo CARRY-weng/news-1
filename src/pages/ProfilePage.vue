c004f1646fc08c29b4834c3373f67d8a60aabc6a
CARRY-weng <53970982+CARRY-weng@users.noreply.github.com>
2020-07-06 12:25:14 +0800

    退出登录

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -34,6 +34,10 @@
     lable='设置'
     desc=''
     @topage='topage'/>
+    <cellBar 
+    lable='退出登录'
+    desc=''
+    @topage='logout'/>
   </div>
 </template>
 
@@ -54,9 +58,21 @@ export default {
     topage(page){
       console.log('跳转到' + page + '页面');
       
+    },
+    logout(){
+      //点击退出登录 要把本地储存的东西删除
+      localStorage.removeItem('token')
+      localStorage.removeItem('user_id')
+      //还要把页面替换为 登录页面 （得用replace）
+      this.$router.replace({
+        path:'/login'
+      })
+
     }
   },
   mounted(){
+    console.log('好了');
+    
     this.$axios({
       url:'/user/'+localStorage.getItem('user_id'),
       method:'get',
